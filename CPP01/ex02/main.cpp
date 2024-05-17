1cd0aff05e9015067598e1c7a55f8e2ec3296e44
rpliego <tetopliego29@gmail.com>
2024-05-17 20:43:31 +0200

    cpp01 ex02 fixed

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -4,10 +4,10 @@ int	main(void)
 {
 	std::string s1 = "HI THIS IS BRAIN";
 	std::string	*s2 = &s1;
-	std::string	s3 = s1;
+	std::string	&s3 = s1;
 
 	std::cout << &s1 << std::endl;
-	std::cout << &s2 << std::endl;
+	std::cout << s2 << std::endl;
 	std::cout << &s3 << std::endl << std::endl;
 
 	std::cout << s1 << std::endl;
