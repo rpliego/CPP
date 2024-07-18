c07653d5a311cf362fd90bf60ef697f03491d36e
rpliego <tetopliego29@gmail.com>
2024-07-18 12:07:26 +0200

    00 doneton

---
+++ CPP07/ex00/src/main.cpp
@@ -0,0 +1,22 @@
+#include "Whatever.hpp"
+#include <iostream>
+
+int	main()
+{
+	int a = 2;
+	int b = 3;
+
+	WHATEVER_HPP::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	
+	WHATEVER_HPP::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
