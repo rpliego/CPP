371e400dfc49f0c9d1b3064a4c9fbf68fa078f8f
rpliego <tetopliego29@gmail.com>
2024-08-20 01:55:13 +0200

    locura

--- CPP09/ex02/src/PmergeMe.cpp
+++ CPP09/ex02/src/PmergeMe.cpp
@@ -30,25 +30,73 @@ std::vector<int> generateJacobN(int size)
 			break ;
 	}
 
-	for (int i = 0; i < (int)arrJbn.size(); i++)
-	{
-		std::cout << arrJbn[i] << std::endl;
-	}
+	// for (int i = 0; i < (int)arrJbn.size(); i++)
+	// {
+	// 	std::cout << arrJbn[i] << std::endl;
+	// }
 	
 	return arrJbn;
 }
 
-void	splitPairVector(std::vector<int>& vec)
+std::vector<std::pair<int, int> >	splitPairVector(std::vector<int>& vec, std::pair<bool, int>& odd)
 {
-	std::vector<int> main;
+	std::vector<std::pair<int, int> > pairs;
+
+	if (vec.size() % 2 != 0)
+	{
+		odd.first = true;
+		odd.second = vec.back();
+		vec.pop_back();
+	}
+
+	for (std::size_t i = 0; i < vec.size(); i += 2)
+		pairs.push_back(std::make_pair(vec[i], vec[i + 1]));
+
+	for (std::size_t i = 0; i < pairs.size(); i++)
+	{
+		if (pairs[i].first > pairs[i].second)
+			std::swap(pairs[i].first, pairs[i].second); 
+		// (pairs[i].first > pairs[i].second) ? std::swap(pairs[i].first, pairs[i].second) : void();
+	}
+	
+	for (std::size_t i = 0; i < pairs.size(); i++)
+	{
+		for (std::size_t j = 0; j < pairs.size(); j++)
+		{
+			if (i != j && pairs[j].second > pairs[i].second)
+			{
+				std::cout << "i: " << i << " j: " << j << std::endl;
+				std::swap(pairs[i], pairs[j]);
+			}
+		}
+	}
+	
+	for (size_t i = 0; i < pairs.size(); ++i)
+        std::cout << "(" << pairs[i].first << ", " << pairs[i].second << ")" << std::endl;
+
+	return pairs;
 }
 
-void	PmergeMe::FordJohnson(std::vector<int> arr)
+void	sortVector(std::vector<std::pair<int, int> > pairs, std::pair<bool, int> odd)
 {
-	std::vector<int> JacobNumbers = generateJacobN(arr.size());
+	(void)pairs;
+	(void)odd;
+	std::vector<int> mainv;
+	std::vector<int> pend;
 
-	std::vector<int> vec;
+	//poner en main los numeros grandes de las parejas y en pend los pequenos;
+}
+
+void	PmergeMe::FordJohnson(std::vector<int> vec)
+{
+	std::vector<int> JacobNumbers = generateJacobN(vec.size());
+
 	std::list<int> list;
+	std::pair<bool, int> odd(false, 0);
 
-	splitPairVector(vec);
+
+	std::vector<std::pair<int, int> > pairs = splitPairVector(vec, odd);
+
+	sortVector(pairs, odd);
+
 }
